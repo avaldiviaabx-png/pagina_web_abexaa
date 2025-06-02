c7cca393fb527869a322e357599b62d2c67a773a
aronvaldivia <59984451+aronvaldivia@users.noreply.github.com>
2025-06-02 17:47:41 +0200

    Updated Preloader.tsx

--- src/components/Preloader.tsx
+++ src/components/Preloader.tsx
@@ -10,7 +10,7 @@ const Preloader: React.FC<PreloaderProps> = ({ isLoading }) => {
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-white">
       <img
-        src="https://mcusercontent.com/c379e3356454ef2a14873d293/images/2c3206bb-89c3-deb2-1e96-61c459fcf55c.png"
+        src="https://mcusercontent.com/c379e3356454ef2a14873d293/images/5ad346a8-12a4-dc92-f032-7045234ab6e2.png"
         alt="Loading..."
         className="w-32 h-32 animate-pulse"
       />
