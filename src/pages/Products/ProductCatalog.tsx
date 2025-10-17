7e9ccda9f2746e740957567a8a5872a847dba794
avaldiviaabx-png <avaldivia.abx@gmail.com>
2025-10-17 18:30:38 +0200

    Updated ProductCatalog.tsx

--- src/pages/Products/ProductCatalog.tsx
+++ src/pages/Products/ProductCatalog.tsx
@@ -8,7 +8,7 @@ export const products = [
     name: ' GPS ABEJA',
     description:'Máquina GPS multifuncional para transporte público e optimizado para obtener el control de frecuencias entre buses de transporte urbano a través de relojes virtuales.', 
     category: 'GPS',
-    model3DUrl: 'https://prod.spline.design/u9fv6rJlGcuCx0mX/scene.splinecode',
+    model3DUrl: 'https://my.spline.design/untitled-u9fv6rJlGcuCx0mXLCRMOg6R/',
     longDescription: 
       'Este equipo está optimizado para obtener el control de frecuencias entre buses de transporte urbano a través de relojes virtuales. Este sistema permite tener un control en línea del recorrido, velocidad y proximidad entre unidades de transporte urbano. ',
       
