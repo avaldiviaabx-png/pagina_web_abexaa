3417c9badb8a3dfd928e4275af8ddd3f64fdeac6
avaldiviaabx-png <avaldivia.abx@gmail.com>
2025-10-20 16:47:42 +0200

    Updated ProductCard.tsx

--- src/pages/Products/ProductCard.tsx
+++ src/pages/Products/ProductCard.tsx
@@ -33,7 +33,7 @@ const ProductCard = ({ id, image, name, description, category }: ProductCardProp
         <div className="mt-auto">
           <button
             onClick={() => navigate(`/products/${id}`)}
-            className="px-4 py-2 w-full bg-gradient-to-r from-blue-700 to-blue-500 text-white rounded transform hover:scale-105 transition duration-300"
+            className="px-4 py-2 w-full bg-gradient-to-r from-blue-600 to-blue-500 text-white rounded transform hover:scale-105 transition duration-300"
           >
             Ver detalles
           </button>
